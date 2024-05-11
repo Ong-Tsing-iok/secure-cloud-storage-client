880eee22725604bdb2e79129d9694878bb831919
Ong-Tsing-iok <86332690+Ong-Tsing-iok@users.noreply.github.com>
2024-05-12 05:13:29 +0800

    Add logging feature

--- src/main/MessageManager.js
+++ src/main/MessageManager.js
@@ -1,10 +1,12 @@
 import io from 'socket.io-client'
+import { getLogger } from './Logger'
 
 const url = 'http://localhost:3001'
 const socket = io(url)
 
 socket.on('message', (message) => {
-  console.log(message)
+  // console.log(message)
+  getLogger().log('info', `received message: ${message}`)
 })
 
 export function sendMessage(message) {
