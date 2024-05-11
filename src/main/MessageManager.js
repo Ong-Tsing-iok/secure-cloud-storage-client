bf497895480d6eab7b7ca4abd00cecd11dae939f
Ong-Tsing-iok <86332690+Ong-Tsing-iok@users.noreply.github.com>
2024-05-12 05:29:09 +0800

    Add error log related to connection failure

--- src/main/MessageManager.js
+++ src/main/MessageManager.js
@@ -2,11 +2,29 @@ import io from 'socket.io-client'
 import { getLogger } from './Logger'
 
 const url = 'http://localhost:3001'
-const socket = io(url)
+const socket = io(url, {
+  reconnectionAttempts: 10
+})
 
 socket.on('message', (message) => {
   // console.log(message)
   getLogger().log('info', `received message: ${message}`)
+})
+
+socket.on('connect_error', (error) => {
+  if (socket.active) {
+    // temporary failure, the socket will automatically try to reconnect
+    getLogger().info('temperal connection failure')
+  } else {
+    // the connection was denied by the server
+    // in that case, `socket.connect()` must be manually called in order to reconnect
+    getLogger().error(`Connedction failed with following error: ${error.message}`)
+    // console.log(error.message);
+  }
+})
+
+socket.io.on('reconnect_failed', () => {
+  getLogger().error('reconnection failed. The server might be down.')
 })
 
 export function sendMessage(message) {
