463db835f07ee73129bc52f34c866f97f3a2743c
Ong-Tsing-iok <86332690+Ong-Tsing-iok@users.noreply.github.com>
2024-11-10 04:41:08 +0800

    Register function and refactor according to server

--- src/main/MessageManager.js
+++ src/main/MessageManager.js
@@ -16,11 +16,11 @@ socket.on('message', (message) => {
 socket.on('connect_error', (error) => {
   if (socket.active) {
     // temporary failure, the socket will automatically try to reconnect
-    // logger.info('temperal connection failure')
+    logger.info("Can't connect to server. Trying to reconnect...")
   } else {
     // the connection was denied by the server
     // in that case, `socket.connect()` must be manually called in order to reconnect
-    logger.error(`Connedction failed with following error: ${error.message}`)
+    logger.error(`connection failed with following error: ${error.message}`)
     // console.log(error.message);
   }
 })
@@ -31,7 +31,7 @@ socket.io.on('reconnect_failed', () => {
 
 socket.io.on('reconnect', () => {
   logger.info('server reconnected')
-  login()
+  // login()
 })
 
 socket.io.on('close', () => {
@@ -41,34 +41,60 @@ socket.io.on('close', () => {
 export function sendMessage(message) {
   socket.emit('message', message)
 }
+
 // Login part
-socket.on('login-res', async (cipher) => {
+async function respondToAuth(cipher) {
   logger.info('Getting login response from server. Decrypting auth key...')
 
   const decryptedValue = await decrypt(cipher)
   logger.debug(`decryptedValue: ${decryptedValue}`)
   logger.info('Finish decrypting. Sending response back to server')
-  socket.emit('login-auth', decryptedValue)
-})
-
-socket.on('login-auth-res', (userId) => {
-  if (userId) {
-    logger.info('Login succeeded')
-    GlobalValueManager.userId = userId
-  } else {
-    logger.warn('Login failed. There might be problem with your keys')
-  }
-})
-
+  socket.emit('auth-res', decryptedValue, (error, userId) => {
+    if (error) {
+      logger.error(`Authentication response failed because of following error: ${error}`)
+      GlobalValueManager.mainWindow?.webContents.send('notice', 'Failed to authenticate', 'error')
+      return
+    }
+    if (userId) {
+      logger.info('Login succeeded')
+      GlobalValueManager.userId = userId
+    }
+  })
+}
 async function login() {
   try {
     await initKeys()
     const publicKey = getPublicKey()
     logger.info('Asking to login...')
-    socket.emit('login-ask', publicKey)
+    socket.emit('login', publicKey, (error, cipher) => {
+      if (error) {
+        logger.error(`login failed because of following error: ${error}`)
+        GlobalValueManager.mainWindow?.webContents.send('notice', 'Failed to login', 'error')
+        return
+      }
+      respondToAuth(cipher)
+    })
   } catch (error) {
     logger.error(`login failed because of following error: ${error}`)
   }
 }
 
-export { login, socket }
+async function register(name, email) {
+  try {
+    await initKeys()
+    const publicKey = getPublicKey()
+    logger.info('Asking to register...')
+    socket.emit('register', publicKey, name, email, (error, cipher) => {
+      if (error) {
+        logger.error(`register failed because of following error: ${error}`)
+        GlobalValueManager.mainWindow?.webContents.send('notice', 'Failed to register', 'error')
+        return
+      }
+      respondToAuth(cipher)
+    })
+  } catch (error) {
+    logger.error(`register failed because of following error: ${error}`)
+  }
+}
+
+export { login, register, socket }
